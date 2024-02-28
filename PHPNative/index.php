1122ccba153f7e71c7eac3cca51959b4ce4fc551
PMM-Yohana <160761091+PMM-Yohana@users.noreply.github.com>
2024-02-28 10:47:42 +0700

    Add files via upload

---
+++ PHPNative/index.php
@@ -0,0 +1,47 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perhitungan Nilai Akhir dan Konversi Nilai</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <b>Perhitungan dan Konversi Nilai<b>
+                    </div>
+                    <div class="card-body">
+                        <form action="hasil.php" method="POST">
+                            <div class="form-group">
+                                <label>Nilai Partisipasi</label>
+                                <input type="text" class="form-control" name="partisipasi" required>
+                            </div>
+                            <div class="form-group">
+                                <label>Nilai Tugas</label>
+                                <input type="text" class="form-control" name="tugas" required>
+                            </div>
+                            <div class="form-group">
+                                <label>Nilai UTS</label>
+                                <input type="text" class="form-control" name="uts" required>
+                            </div>
+                            <div class="form-group">
+                                <label>Nilai UAS</label>
+                                <input type="text" class="form-control" name="uas" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Konversi</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
